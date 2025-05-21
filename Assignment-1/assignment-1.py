9760cf2e51a18f2de43d060236e3498bc7e50fb2
vishwak05 <vishwaksai524@gmail.com>
2025-05-21 14:50:23 +0530

    Assignment-1

--- Assignment-1/assignment-1.py
+++ Assignment-1/assignment-1.py
@@ -1,13 +1,7 @@
 # Printing the unique words from a input text file in sorted order
 
-# Opening 'input.txt' in read mode
-input_file = open('input.txt', 'r')
-
-# Reading the contents of input file
-input_txt = input_file.read()
-
-print('The input text is:')
-print(f'"{input_txt}"')
+# Reading the input text
+input_txt = input("Provide the input here: ")
 
 # Spliting the String into words by " " white space
 # Creating a set of all splitted words
@@ -17,7 +11,4 @@ words = sorted(set(input_txt.split(" ")))
 # Joining the sorted words into a single String
 joined_words = " ".join(words)
 print("\nThe unique words from input in a sorted order:")
-print(f'"{joined_words}"')
-
-# Closing the input text file
-input_file.close()
+print(joined_words)
